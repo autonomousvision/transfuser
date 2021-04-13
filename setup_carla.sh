62086ef70563a09b30702bbe600267d9be2f398e
Aditya Prakash <adityaprakash229997@gmail.com>
2021-04-13 18:17:32 +0200

    download and install CARLA

---
+++ setup_carla.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Download and install CARLA
+mkdir carla
+cd carla
+wget https://carla-releases.s3.eu-west-3.amazonaws.com/Linux/CARLA_0.9.10.1.tar.gz
+wget https://carla-releases.s3.eu-west-3.amazonaws.com/Linux/AdditionalMaps_0.9.10.1.tar.gz
+tar -xf CARLA_0.9.10.1.tar.gz
+tar -xf AdditionalMaps_0.9.10.1.tar.gz
+rm CARLA_0.9.10.1.tar.gz
+rm AdditionalMaps_0.9.10.1.tar.gz
+cd ..
