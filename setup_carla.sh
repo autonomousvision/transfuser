6d1d0421598e3767ced97a113739a40a621cdc25
Bernhard <B.Jaeger1@gmx.net>
2024-05-14 14:10:39 +0200

    Update CARLA download link in cvpr2021 branch.

--- setup_carla.sh
+++ setup_carla.sh
@@ -3,8 +3,8 @@
 # Download and install CARLA
 mkdir carla
 cd carla
-wget https://carla-releases.s3.eu-west-3.amazonaws.com/Linux/CARLA_0.9.10.1.tar.gz
-wget https://carla-releases.s3.eu-west-3.amazonaws.com/Linux/AdditionalMaps_0.9.10.1.tar.gz
+wget https://carla-releases.s3.us-east-005.backblazeb2.com/Linux/CARLA_0.9.10.1.tar.gz
+wget https://carla-releases.s3.us-east-005.backblazeb2.com/Linux/AdditionalMaps_0.9.10.1.tar.gz
 tar -xf CARLA_0.9.10.1.tar.gz
 tar -xf AdditionalMaps_0.9.10.1.tar.gz
 rm CARLA_0.9.10.1.tar.gz
